72c13f2ee0a34091f9b2cf4a29b7ea3cbbb25103
Shahd Abu Sharif <eng.shahd.abusharif@gmail.com>
2025-07-30 14:48:39 +0300

    Update BaseRepo.ts

--- src/BaseRepo.ts
+++ src/BaseRepo.ts
@@ -7,35 +7,45 @@ export class BaseRepository<T extends BaseModel> implements interface_repo<T> {
     this.items = initialItems
   }
 
+    // retrieve all entities
     async getAll(): Promise<T[]> {
     return this.items
   }
 
+    // retreieve entity by id
     async getById(id: number): Promise<T | undefined> {
     return this.items.find(item => item.id === id)
   }
 
-
+    
+    // create new entity
     async create(item: T): Promise<T> {
     this.items.push(item)
     return item
   }
 
 
+    // update entiyt based on the id number
     async update(id: number, item: Partial<T>): Promise<T | undefined> {
-    const index = this.items.findIndex(i => i.id === id)
-    if (index === -1) return undefined
-    this.items[index] = { ...this.items[index], ...item }
-    return this.items[index]
+    const existing = this.items.find(item => item.id === id)
+    if (!existing) return undefined
+      
+    const updatedItem = { ...existing, ...item, id }
+    this.items = this.items.map(item => item.id === id ? updatedItem : item)
+
+  return updatedItem
+
+    
   }
 
 
+    // delete entity based on id number
     async delete(id: number): Promise<boolean> {
-    const index = this.items.findIndex(i => i.id === id)
-    if (index === -1) return false
-    this.items.splice(index, 1)
-    return true
+      const lengthBefore = this.items.length;
+     this.items = this.items.filter(item => item.id !== id);
+      return this.items.length < lengthBefore;
   }
+
 
 
     async filter(query: Partial<T>): Promise<T[]> {
